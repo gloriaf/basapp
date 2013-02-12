72f9a48be6e9e95f7fc857955a7942c7d2c32aed
gloriaf <mgfernandez@pabilos.com>
2013-02-12 18:34:47 +0100

    modificacion de projects

--- db/migrate/20130212154235_create_projects.rb
+++ db/migrate/20130212154235_create_projects.rb
@@ -2,7 +2,7 @@ class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :type
+      t.string :project_type
       t.date :start
       t.date :end
       t.text :description
