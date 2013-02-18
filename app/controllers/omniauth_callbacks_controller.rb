ed83c74892fe907775a1a5ac1428319e900ff96c
gloriaf <mgfernandez@pabilos.com>
2013-02-18 16:27:56 +0100

    registro de usuarios a traves de terceros -correccion

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,13 +1,14 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def all
+#    logger.debug "--------> auth: #{request.env["omniauth.auth"]}"
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = "Signed in!"
-      sign_in_and_redirect user, :locale => params[:locale]
+      sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
-      redirect_to new_user_registration_url, :locale => params[:locale]
+      redirect_to new_user_registration_url
     end
   end
   
